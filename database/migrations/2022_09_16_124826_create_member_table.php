20d987a2cb98cd945913162b982fd0878afd01ce
orangeandme <3378335470@qq.com>
2022-09-16 19:00:10 +0800

    首次commit

---
+++ database/migrations/2022_09_16_124826_create_member_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //建表
+        Schema::create('member',function($table){
+            $table -> increments('id');
+            $table -> string('username',20) -> notNull();
+            $table -> string('password') -> notNull();
+            $table -> enum('gender',[1,2,3]) -> notNull() -> default('1');
+            $table -> string('mobile',11);
+            $table -> string('email',40);
+            $table -> string('avatar');
+            $table -> timestamps();
+            $table -> rememberToken();
+            $table -> enum('type',[1,2]) -> notNull() -> default('1');
+            $table -> enum('status',[1,2]) -> notNull() -> default('2');   
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//后台路由部分（不需要权限判断）
+Route::group(['prefix' => 'admin'],function(){
+	//后台登录页面
+	Route::get('public/login','Admin\PublicController@login') -> name('login');
+	//后台登录处理页面
+	Route::post('public/check','Admin\PublicController@check');
+	//退出地址
+	Route::get('public/logout','Admin\PublicController@logout');
+});
+
+//后台路由部分（需要权限判断）
+Route::group(['prefix' => 'admin','middleware' => ['auth:admin','checkrbac']],function(){
+	//后台首页路由
+	Route::get('index/index','Admin\IndexController@index');
+	Route::get('index/welcome','Admin\IndexController@welcome');
+
+	//管理员的管理模块
+	Route::get('manager/index','Admin\ManagerController@index');
+
+	//权限的管理模块
+	Route::get('auth/index','Admin\AuthController@index');
+	Route::any('auth/add','Admin\AuthController@add');
+
+	//角色的管理模块
+	Route::get('role/index','Admin\RoleController@index');
+	Route::any('role/assign','Admin\RoleController@assign');
+
+	//会员的管理模块
+	Route::get('member/index','Admin\MemberController@index');//列表
+	Route::any('member/add','Admin\MemberController@add');//添加
+	Route::post('uploader/webuploader','Admin\UploaderController@webuploader');//异步上传
+	Route::get('member/getareabyid','Admin\MemberController@getAreaById');//ajax联动
+});
+	
